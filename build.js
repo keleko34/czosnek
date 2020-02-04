d4ccd103649d9795bf865ceb87e0cb4d724c922e
= <gregory.guidero@gmail.com>
2020-02-04 13:36:51 +0100

    Fix build scrip from upgraded closure compiler

--- build.js
+++ build.js
@@ -1,13 +1,15 @@
-var base = process.cwd().replace(/\\/g,'/'),
-    fs = require('fs'),
-    closureCompiler = require('google-closure-compiler-js').compile,
-    flags = {};
+const { unlink, readFileSync, writeFileSync } = require('fs')
+      base = process.cwd().replace(/\\/g,'/'),
+      closureCompiler = require('google-closure-compiler-js'),
+      flags = {};
 
 console.log("Building Czosnek Library...");
 
-flags.jsCode = [{src: fs.readFileSync(base+'/czosnek.js','utf8')}];
+flags.jsCode = [{src: readFileSync(base+'/czosnek.js','utf8')}];
 flags.compilationLevel = 'SIMPLE';
-fs.unlinkSync(base+'/czosnek.min.js');
-fs.writeFileSync(base+'/czosnek.min.js',closureCompiler(flags).compiledCode);
-
-console.log("Finished Building Minified Library..");
+flags.rewritePolyfills = false;
+unlink(base+'/czosnek.min.js', (err) => {
+ if(err && !err.code === 'ENOENT') return;
+ writeFileSync(base+'/czosnek.min.js',closureCompiler(flags).compiledCode);
+ console.log("Finished Building Minified Czosnek Library..");
+});
